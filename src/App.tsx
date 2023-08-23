2537481263dfd4552b486a737ea542d43c972463
unknown <fefe.borrometi@gmail.com>
2023-08-23 17:28:00 +0200

    chore: react router import and routes config

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
+// import router
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
 // import components
 import Home from "./pages/home/Home";
+import Users from "./pages/users/Users";
+import Products from "./pages/products/Products";
 
 function App() {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+  // router
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "users",
+      element: <Users />,
+    },
+    {
+      path: "products",
+      element: <Products />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
