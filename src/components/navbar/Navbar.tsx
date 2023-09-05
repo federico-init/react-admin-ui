63104fe6baf57249d197d1b3571ebae61a56fb4f
unknown <fefe.borrometi@gmail.com>
2023-09-05 18:03:27 +0200

    fix: topBox style and img alts added in navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,31 +1,29 @@
 // import style
 import "./navbar.scss";
 
-// TODO: add alts to img
-
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="logo">
-        <img src="/logo.svg" alt="" />
+        <img src="/logo.svg" alt="logo" />
         <span>adminboard</span>
       </div>
       <div className="icons">
-        <img src="/search.svg" alt="" className="icon" />
-        <img src="/app.svg" alt="" className="icon" />
-        <img src="/expand.svg" alt="" className="icon" />
+        <img src="/search.svg" alt="search icon" className="icon" />
+        <img src="/app.svg" alt="app icon" className="icon" />
+        <img src="/expand.svg" alt="expand icon" className="icon" />
         <div className="notification">
-          <img src="/notifications.svg" alt="" />
+          <img src="/notifications.svg" alt="notification icon" />
           <span>1</span>
         </div>
         <div className="user">
           <img
             src="https://images.pexels.com/photos/11038549/pexels-photo-11038549.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
-            alt=""
+            alt="user image"
           />
           <span>Jane</span>
         </div>
-        <img src="/settings.svg" alt="" className="icon" />
+        <img src="/settings.svg" alt="settings icon" className="icon" />
       </div>
     </div>
   );
